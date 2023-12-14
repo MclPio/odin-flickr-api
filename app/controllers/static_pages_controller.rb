c21572395624785688e587a84e47e98421f043c0
MclPio <113801014+MclPio@users.noreply.github.com>
2023-12-14 14:50:25 -0500

    Implement api via index action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
   def index
+    flickr_api_key = Figaro.env.flickr_api_key
+    flickr_shared_secret = Figaro.env.flickr_shared_secret
+    flickr = Flickr.new(flickr_api_key, flickr_shared_secret)
+    @user_photos = flickr.photos.search(user_id: flickr_params, format: 'json')
+    # @user_photos is accessable through parameters. ex: user_photos.each { |photo| puts photo.id}
   end
 
-  def show
+  private
+
+  def flickr_params
+    params.require(:id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :static_pages, only: [:index, :show]
+  resources :static_pages
 
   root "static_pages#index"
 end
