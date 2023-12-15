03d421fca4c3499039052f4b6cd70a54c30a4ab3
MclPio <113801014+MclPio@users.noreply.github.com>
2023-12-15 18:16:32 -0500

    Add cssbundling bootstrap, found issues with blank or incorrect id

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,7 +8,10 @@ class StaticPagesController < ApplicationController
       @user_photos = flickr.photos.search(user_id: flickr_params, format: 'json')
     end
   end
-
+  #Issues
+  #1. Form can not handle id of 0 'flickr.photos.search' - Parameterless searches have been disabled. Please use flickr.photos.getRecent instead.
+  #2. Wrong IDS result in errors 'flickr.photos.search' - Unknown user
+  #3. flickr_params breaks if blank id is submitted. param is missing or the value is empty: id
   private
 
   def flickr_params
