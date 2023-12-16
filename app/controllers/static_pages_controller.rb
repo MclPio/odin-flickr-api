c3afe6fd52f5143216727842718b95500ea65967
MclPio <113801014+MclPio@users.noreply.github.com>
2023-12-15 19:38:18 -0500

    Fixed controller issues, images will have a height of 400 px by default

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,23 @@
 class StaticPagesController < ApplicationController
   def index
-    @user_photos = nil
     flickr_api_key = Figaro.env.flickr_api_key
     flickr_shared_secret = Figaro.env.flickr_shared_secret
     flickr = Flickr.new(flickr_api_key, flickr_shared_secret)
-    if !params[:id].nil?
-      @user_photos = flickr.photos.search(user_id: flickr_params, format: 'json')
+
+    begin
+      if params[:commit] == "Submit"
+        user_id = flickr_params
+        @user_photos = flickr.photos.search(user_id: user_id, format: 'json')
+      end
+    rescue Flickr::FailedResponse
+      flash[:alert] = "ID is invalid"
+      @user_photos = []
     end
   end
-  #Issues
-  #1. Form can not handle id of 0 'flickr.photos.search' - Parameterless searches have been disabled. Please use flickr.photos.getRecent instead.
-  #2. Wrong IDS result in errors 'flickr.photos.search' - Unknown user
-  #3. flickr_params breaks if blank id is submitted. param is missing or the value is empty: id
+
   private
 
   def flickr_params
-    params.require(:id)
+    params.permit(:id)[:id] || ""
   end
 end
